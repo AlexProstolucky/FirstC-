8deeb8c8005827780b747e9286c00cb17ecdfcd8
AlexProstolucky <alexprostolucky@gmail.com>
2023-03-27 18:16:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+internal class Program
+{
+    private static void Main()
+    {
+        int A, B;
+        A = Convert.ToInt32(ReadLine());  
+        B = Convert.ToInt32(ReadLine());
+        for (int i = A; i < B + 1; i++)
+        {
+            for (int j = 0; j < A; j++)
+            {
+                Write(A);
+            }
+            A++;
+            WriteLine("");
+        }
+    }
+}
